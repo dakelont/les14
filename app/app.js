0edd2e53e699363ca7b4e03c6cd9b2c922dddcd9
dakelont <bk-82-82@bk.ru>
2017-11-22 17:49:42 +0300

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -2,14 +2,38 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.myaccount',
-  'myApp.version',
-  'myMenu'
+    'ui.router',
+    'ngMessages'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config(function($stateProvider) {
+    $stateProvider
+        .state({
+            name: 'list',
+            url: '/list',
+            templateUrl: 'PokemonList/PokemonList.html',
+            controller: 'PokemonListCtrl as vm'
+        })
+        .state({
+            name: 'createNewPokemon',
+            url: '/new',
+            templateUrl: 'CreatePokemon/CreatePokemon.html',
+            controller: 'CreatePokemonCtrl as vm'
+        })
+        .state({
+            name: 'detail',
+            url: '/pokemons/:pokemonId',
+            templateUrl: 'PokemonDetail/PokemonDetail.html',
+            controller: 'PokemonDetailCtrl as vm'
+        }).state({
+            name: 'detail.edit',
+            url: '/edit',
+            parent: 'detail',
+            templateUrl: 'PokemonDetail/PokemonDetailEdit.html',
+            controller: function() {}
+        }).state({
+            name: 'myaccount',
+            url: '/myaccount',
+            templateUrl: 'myaccount/myaccount.html',
+            controller: 'myaccountCtrl as vm'
+        });
+});
--- app/myMenu.js
+++ app/myMenu.js
@@ -1,23 +1,20 @@
 'use strict';
-angular.module('myMenu', [])
-.controller('myMenuCtrl', function ($scope, $location) {
-    $scope.menu = [
-		{
-			'id':'view1',
-			'name':'view1'
-		},
-		{
-			'id':'view2',
-			'name':'view2'
-		},
-		{
-			'id':'myaccount',
-			'name':'myaccount'
-		}
-	];
-	
-	$scope.isActive = function(location) {
-		return '/' + location === $location.path()
-	}
-
-});
+angular
+	.module('myApp')
+	.controller('myMenuCtrl', function () {
+		var vm = this;
+		vm.menu = [
+			{
+				'id':'list',
+				'name':'Список'
+			},
+			{
+				'id':'createNewPokemon',
+				'name':'Добавить нового'
+			},
+			{
+				'id':'myaccount',
+				'name':'myaccount'
+			}
+		];
+	});
