e4e43566aeafb18bae5b27b0bb09e97a358a78b4
dakelont <bk-82-82@bk.ru>
2017-11-22 17:50:59 +0300

    Add files via upload

--- app/myaccount/myaccount.js
+++ app/myaccount/myaccount.js
@@ -1,20 +1,14 @@
 'use strict';
 
-angular.module('myApp.myaccount', ['ngRoute'])
-
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/myaccount', {
-      templateUrl: 'myaccount/myaccount.html',
-      controller: 'myaccountCtrl'
-    });
-  }])
-
-  .controller('myaccountCtrl', ['$scope', function($scope) {
-        $scope.save = function (answer, answerForm){
-            if(answerForm.$valid){
-                // действия по сохранению
-                console.log(answer);
-                alert(answer.name + ", ваш запрос сохранен");
-            }
-        };
-  }]);
+angular
+  .module('myApp')
+  .controller('myaccountCtrl', function() {
+    var vm = this;
+     vm.save = function (answer, answerForm){
+      if(answerForm.$valid){
+      // действия по сохранению
+        console.log(answer);
+        alert(answer.name + ", ваш запрос сохранен");
+      }
+    };
+  });
